f67a24c21284484e170afe26b601065dec2fdabd
Don Kackman <dkackman@gmail.com>
2024-10-20 22:43:28 -0500

    fix on change input binding error

--- scripts/ui.js
+++ scripts/ui.js
@@ -34,9 +34,8 @@ $(document).ready(function () {
   restoreSelections();
 
   // Save selections and properties when changed
-  $(
-    "#model-selection-1, #model-selection-2, #agent-properties-1, #agent-properties-2"
-  ).on("change input", saveSelections);
+  $("#model-selection-1, #model-selection-2, #agent-properties-1, #agent-properties-2, #agent-opening-message-1")
+    .on("change input", saveSelections);
 
   // Add click handlers for the buttons
   $("#load-models").on("click", loadModels);
